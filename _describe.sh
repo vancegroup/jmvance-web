69823581765cc2136ed0924b57b3ec6bc86b92bb
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-19 15:29:14 -0600

    Update how we describe.

--- _describe.sh
+++ _describe.sh
@@ -5,9 +5,9 @@ cd $(dirname $0)
 if [ "$1" == "--apply" ]; then
   cp _config.yml _config.yml.bak
   cat >> _config.yml <<EOF
-commitid: $(git describe --always)
-commitlinktext: "$(git log -n 1 --pretty=format:'%h - %an, %ar.')"
-commitdesc: "$(git log -n 1 --pretty=format:'%B')"
+commitid: $(git log -n 1 --pretty=format:'%H')
+commitlinktext: "$(git log -n 1 --pretty=format:'%h - %an, %ar - %s')"
+commitdesc: "$(git log -n 1 --pretty=format:'%b')"
 EOF
 elif [ "$1" == "--unapply" ]; then
   mv _config.yml.bak _config.yml
