d0ac17f91073dda1b964e316e30e504d802893d0
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-19 14:10:53 -0600

    Script to get git details and put it in the readme.

---
+++ _describe.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+(
+cd $(dirname $0)
+if [ "$1" == "--apply" ]; then
+  cp _config.yml _config.yml.bak
+  cat >> _config.yml <<EOF
+commitid: $(git describe --always)
+commitlinktext: $(git log -n 1 --pretty=format:"%h: %an, %ar.")
+commitdesc: git log -n 1 --pretty=format:"%B"
+EOF
+elif [ "$1" == "--unapply"]; then
+  mv _config.yml.bak _config.yml
+else
+  echo "Error - must pass --apply or --unapply !"
+  exit 1
+fi
+)
