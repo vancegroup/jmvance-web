248cb3084ab25a470a1fc3265fd3e894f59fac4d
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-22 16:24:46 -0600

    Add comments to mappings file

--- _scripts/config/mappings.lua
+++ _scripts/config/mappings.lua
@@ -1,4 +1,9 @@
 mappings = {
+	-- These pages are still pointed to by some old reachable pages, so the
+	-- redirect is mandatory to not break things.
 	["/htmls/research.htm"] = "/RESEARCH/";
+
+	-- There should be no more internal links to these pages, so the mapping
+	-- is perhaps overkill but playing it safe (and keeping Google Juice)
 	["/RESEARCH/COMPLIANT/compliant_mechanism_design.html"] = "/RESEARCH/COMPLIANT/";
 }
