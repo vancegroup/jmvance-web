fdc1ec790125984921d4e15f63a51187a783f803
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-22 15:28:46 -0600

    Move lua scripts

--- _scripts/common.sh
+++ _scripts/common.sh
@@ -16,12 +16,13 @@ if [ -z "${COMMON_RUN_FLAG}" ]; then
 
 	export REPOROOT=$(cd $(dirname "${BASH_SOURCE}") && cd .. && pwd)
 	export SCRIPTS=${REPOROOT}/_scripts
+	export LUASCRIPTS=${SCRIPTS}/lua
 	export DESCRIBE=${SCRIPTS}/describe
 	export MAPPINGS=${SCRIPTS}/mappings.lua
 
 	new_lua_loc=${SCRIPTS}/config/?.lua
 	[ -z "${LUA_PATH}" ] && export LUA_PATH=$new_lua_loc || export LUA_PATH=$new_lua_loc:${LUA_PATH}
-	export PATH=${SCRIPTS}:$PATH
+	export PATH=${SCRIPTS}:${LUASCRIPTS}:$PATH
 
 	export COMMON_RUN_FLAG=true
 fi
