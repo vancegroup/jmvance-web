64c393ce95e3cecf81ce60d3f4a8baf3d60562b5
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-22 15:29:40 -0600

    Clean up and improve error checking, messages.

--- _scripts/common.sh
+++ _scripts/common.sh
@@ -18,7 +18,6 @@ if [ -z "${COMMON_RUN_FLAG}" ]; then
 	export SCRIPTS=${REPOROOT}/_scripts
 	export LUASCRIPTS=${SCRIPTS}/lua
 	export DESCRIBE=${SCRIPTS}/describe
-	export MAPPINGS=${SCRIPTS}/mappings.lua
 
 	new_lua_loc=${SCRIPTS}/config/?.lua
 	[ -z "${LUA_PATH}" ] && export LUA_PATH=$new_lua_loc || export LUA_PATH=$new_lua_loc:${LUA_PATH}
