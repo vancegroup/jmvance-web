5e2032445796e1061201fa1ddc870f19fc465485
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-22 15:01:56 -0600

    Add HTACCESS as a config variable

--- _scripts/common.sh
+++ _scripts/common.sh
@@ -1,7 +1,11 @@
 echo "* common.sh"
 if [ -z "${COMMON_RUN_FLAG}" ]; then
 	echo "** first time for common.sh"
-	if [ -z "${DOMAIN}" -o -z "${WEBROOT}"  -o -z "${FILEROOT}" -o -z "${CONFIG}" ]; then
+	if [ -z "${DOMAIN}" -o \
+	     -z "${WEBROOT}" -o \
+	     -z "${FILEROOT}" -o \
+	     -z "${HTACCESS}" -o \
+	     -z "${CONFIG}" ]; then
 		echo "Must source a configuration file using run-with-config!" >&2; exit 1
 	fi
 
@@ -14,7 +18,7 @@ if [ -z "${COMMON_RUN_FLAG}" ]; then
 	export SCRIPTS=${REPOROOT}/_scripts
 	export DESCRIBE=${SCRIPTS}/describe
 	export MAPPINGS=${SCRIPTS}/mappings.lua
-	
+
 	new_lua_loc=${SCRIPTS}/?.lua
 	[ -z "${LUA_PATH}" ] && export LUA_PATH=$new_lua_loc || export LUA_PATH=$new_lua_loc:${LUA_PATH}
 	export PATH=${SCRIPTS}:$PATH
