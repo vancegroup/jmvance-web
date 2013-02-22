05970db64929c5409f6622e315d235749bd25819
Ryan Pavlik <rpavlik@iastate.edu>
2013-02-22 15:19:59 -0600

    Move full deploy script out.

--- _full_deploy.sh
+++ _full_deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
 
-SCRIPTDIR=$(cd $(dirname $0) && pwd)
+SCRIPTDIR=$(cd $(dirname $0) && cd _scripts && pwd)
 CONFIG=$1
+
+[ ! -z "$CONFIG" ] || echo "Need config passed as first argument!" >&2; exit 1
+
 echo "- Installing bundled gems"
 "${SCRIPTDIR}/bundle-install"
 
