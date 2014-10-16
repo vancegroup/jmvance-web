a107d08fff64971e210e63bfbc1402c4c2150cf1
Ryan Pavlik <rpavlik@iastate.edu>
2014-10-16 15:33:11 -0500

    Update mappings.lua
    Make redirections from old students page to new one.
    
    @carlsonp - this would be the other thing you'd need to update.

--- _scripts/config/mappings.lua
+++ _scripts/config/mappings.lua
@@ -6,6 +6,10 @@ mappings = {
 	-- These pages are still pointed to by some old reachable pages, so the
 	-- redirect is mandatory to not break things.
 	["/htmls/publicat.htm"] = "/PUBLICATIONS/";
+	
+	-- These pages are still pointed to by some old reachable pages, so the
+	-- redirect is mandatory to not break things.
+	["/htmls/students.htm"] = "/STUDENTS/";
 
 	-- There should be no more internal links to these pages, so the mapping
 	-- is perhaps overkill but playing it safe (and keeping Google Juice)
